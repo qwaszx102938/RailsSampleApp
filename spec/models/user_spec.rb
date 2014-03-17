ef0c2f4304ea2556d1f35bdbcd55414502d1195d
william <willy.xiao1989@gmail.com>
2014-03-17 21:25:37 +0800

    model half 1

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe User do
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+  before { @user = User.new name:"Example User" ,email:"Example@email.com" }
+  subject{@user}
+  it{ should respond_to(:name) }
+  it{should respond_to(:email) }
+end
